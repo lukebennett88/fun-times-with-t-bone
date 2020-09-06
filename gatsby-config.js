1643d0ae043d5814b2c99c5ff39ba8c2d8d78b5a
Luke Bennett <hello@lukebennett.com.au>
2020-09-06 10:50:20 +1000

    Convert backticks to single quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,30 +8,31 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`,
+    title: 'Fun Times With Tbones',
+    description: '',
+    author: '',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
-        start_url: `/`,
+        name: 'gatsby-starter-tailwind',
+        short_name: 'starter',
+        start_url: '/',
         background_color: fullConfig.theme.colors.white,
+        theme_color: fullConfig.theme.colors.green['200'],
+        display: 'minimal-ui',
         theme_color: fullConfig.theme.colors.teal['400'],
         display: `minimal-ui`,
         icon: `src/images/tailwind-icon.png`,
       },
     },
     {
-      resolve: `gatsby-plugin-postcss`,
+      resolve: 'gatsby-plugin-postcss',
       options: {
         postCssPlugins: [tailwind(tailwindConfig), autoprefixer],
       },
     },
-    `gatsby-plugin-offline`,
   ],
 };
